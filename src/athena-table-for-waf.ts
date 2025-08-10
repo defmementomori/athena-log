cfd04c4606d3b11e5352a28ea41bdfba771d5857
defmementomori <defmementomori@gmail.com>
2025-08-10 16:21:54 +0900

    fix: waf

--- src/athena-table-for-waf.ts
+++ src/athena-table-for-waf.ts
@@ -9,68 +9,55 @@ export interface AthenaTableForWafProps {
   /** 作成するGlueテーブル名 */
   readonly tableName: string;
   /**
-   * パーティション射影の開始日 (yyyy/MM/dd形式)
-   * @example '2025/08/01'
-   */
-  readonly projectionStartDate: string;
-  /**
-   * ★ WAFのスコープを指定します。
+   * WAFのスコープを指定します。
    * 'REGIONAL' または 'CLOUDFRONT'
    */
   readonly wafScope: 'REGIONAL' | 'CLOUDFRONT';
+  /** クエリ対象のWeb ACL名 */
+  readonly webAclName: string;
   /** バケット内のオプションのプレフィックス */
   readonly logPrefix?: string;
 }
 
+/**
+ * S3に保存されたAWS WAFログをクエリするためのGlueテーブルを作成します。
+ * このコンストラクトは、公式ドキュメントのdate型パーティション射影に準拠します。
+ */
 export class AthenaTableForWaf extends Construct {
   public readonly table: glue.CfnTable;
 
   constructor(scope: Construct, id: string, props: AthenaTableForWafProps) {
     super(scope, id);
 
-    const { logBucketName, databaseName, tableName, projectionStartDate, logPrefix, wafScope } = props;
+    const { logBucketName, databaseName, tableName, wafScope, webAclName, logPrefix } = props;
 
     const stack = Stack.of(this);
     const account = stack.account;
 
-    const logRegion = wafScope === 'CLOUDFRONT' ? 'global' : stack.region;
+    const logRegion = wafScope === 'CLOUDFRONT' ? 'cloudfront' : stack.region;
 
     const s3Prefix = logPrefix ? `${logPrefix}/` : '';
-    const s3BaseLocation = `s3://${logBucketName}/${s3Prefix}AWSLogs/`;
-
-    const s3LocationTemplate = `${s3BaseLocation}${account}/WAFLogs/${logRegion.toLowerCase()}/\${webacl}/\${year}/\${month}/\${day}/\${hour}/\${minute}/`;
+    const s3BaseLocation = `s3://${logBucketName}/${s3Prefix}AWSLogs/${account}/WAFLogs/${logRegion}/${webAclName}/`;
+    const s3LocationTemplate = `${s3BaseLocation}\${log_time}`;
 
     this.table = new glue.CfnTable(this, 'Default', {
       catalogId: account,
       databaseName: databaseName,
       tableInput: {
         name: tableName,
-        description: 'Table for querying AWS WAF logs with partition projection',
         tableType: 'EXTERNAL_TABLE',
         parameters: {
           'projection.enabled': 'true',
-          'projection.webacl.type': 'injected',
-          'projection.year.type': 'integer',
-          'projection.year.range': `${projectionStartDate.substring(0, 4)},2200`,
-          'projection.month.type': 'integer',
-          'projection.month.range': '01,12',
-          'projection.day.type': 'integer',
-          'projection.day.range': '01,31',
-          'projection.hour.type': 'integer',
-          'projection.hour.range': '00,23',
-          'projection.minute.type': 'integer',
-          'projection.minute.range': '00,59',
+          'projection.log_time.type': 'date',
+          'projection.log_time.format': 'yyyy/MM/dd/HH/mm',
+          'projection.log_time.range': '2025/01/01/00/00,NOW',
+          'projection.log_time.interval': '1',
+          'projection.log_time.interval.unit': 'MINUTES',
           'storage.location.template': s3LocationTemplate,
         },
-        partitionKeys: [
-          { name: 'webacl', type: 'string' },
-          { name: 'year', type: 'string' },
-          { name: 'month', type: 'string' },
-          { name: 'day', type: 'string' },
-          { name: 'hour', type: 'string' },
-          { name: 'minute', type: 'string' },
-        ],
+        partitionKeys: [{ name: 'log_time', type: 'string' }],
         storageDescriptor: {
+          // 公式ドキュメントのスキーマ定義に更新
           columns: [
             { name: 'timestamp', type: 'bigint' },
             { name: 'formatversion', type: 'int' },
@@ -78,21 +65,37 @@ export class AthenaTableForWaf extends Construct {
             { name: 'terminatingruleid', type: 'string' },
             { name: 'terminatingruletype', type: 'string' },
             { name: 'action', type: 'string' },
-            { name: 'terminatingrulematchdetails', type: 'array<struct<conditiontype:string,location:string,matcheddata:array<string>>>' },
-            { name: 'nonterminatingmatchingrules', type: 'array<struct<ruleid:string,action:string,rulematchdetails:array<struct<conditiontype:string,location:string,matcheddata:array<string>>>>>' },
-            { name: 'requestheadersinserted', type: 'array<struct<name:string,value:string>>' },
-            { name: 'responsecodesent', type: 'int' },
-            { name: 'labels', type: 'array<struct<name:string>>' },
+            { name: 'terminatingrulematchdetails', type: 'array<struct<conditiontype:string,sensitivitylevel:string,location:string,matcheddata:array<string>>>' },
+            { name: 'httpsourcename', type: 'string' },
+            { name: 'httpsourceid', type: 'string' },
             {
-              name: 'httpsys_request',
-              type: 'struct<clientip:string,country:string,headers:array<struct<name:string,value:string>>,uri:string,args:string,httpversion:string,httpmethod:string,requestid:string>',
+              name: 'rulegrouplist',
+              type: 'array<struct<rulegroupid:string,terminatingrule:struct<ruleid:string,action:string,rulematchdetails:array<struct<conditiontype:string,sensitivitylevel:string,location:string,matcheddata:array<string>>>>,nonterminatingmatchingrules:array<struct<ruleid:string,action:string,overriddenaction:string,rulematchdetails:array<struct<conditiontype:string,sensitivitylevel:string,location:string,matcheddata:array<string>>>,challengeresponse:struct<responsecode:string,solvetimestamp:string>,captcharesponse:struct<responsecode:string,solvetimestamp:string>>>,excludedrules:string>>',
             },
+            { name: 'ratebasedrulelist', type: 'array<struct<ratebasedruleid:string,limitkey:string,maxrateallowed:int>>' },
+            {
+              name: 'nonterminatingmatchingrules',
+              type: 'array<struct<ruleid:string,action:string,rulematchdetails:array<struct<conditiontype:string,sensitivitylevel:string,location:string,matcheddata:array<string>>>,challengeresponse:struct<responsecode:string,solvetimestamp:string>,captcharesponse:struct<responsecode:string,solvetimestamp:string>>',
+            },
+            { name: 'requestheadersinserted', type: 'array<struct<name:string,value:string>>' },
+            { name: 'responsecodesent', type: 'string' },
+            {
+              name: 'httprequest',
+              type: 'struct<clientip:string,country:string,headers:array<struct<name:string,value:string>>,uri:string,args:string,httpversion:string,httpmethod:string,requestid:string,fragment:string,scheme:string,host:string>',
+            },
+            { name: 'labels', type: 'array<struct<name:string>>' },
+            { name: 'captcharesponse', type: 'struct<responsecode:string,solvetimestamp:string,failurereason:string>' },
+            { name: 'challengeresponse', type: 'struct<responsecode:string,solvetimestamp:string,failurereason:string>' },
+            { name: 'ja3fingerprint', type: 'string' },
+            { name: 'ja4fingerprint', type: 'string' },
+            { name: 'oversizefields', type: 'string' },
+            { name: 'requestbodysize', type: 'int' },
+            { name: 'requestbodysizeinspectedbywaf', type: 'int' },
           ],
           location: s3BaseLocation,
           serdeInfo: {
             serializationLibrary: 'org.openx.data.jsonserde.JsonSerDe',
           },
-          compressed: false,
         },
       },
     });
